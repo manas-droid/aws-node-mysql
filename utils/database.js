800d468f74c682cf82b03fcb0a95b857c0ef195a
manas-droid <manas.kalangan@gmail.com>
2021-05-18 16:19:37 +0530

    SSL test1

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 const fs = require('fs');
 require("dotenv").config();
-console.log(__dirname);
 
 const pool = mysql.createPool({
   host  : process.env.AWS_RDS_HOST_NAME,
@@ -9,8 +8,8 @@ const pool = mysql.createPool({
   database :process.env.AWS_RDS_DATABASE_NAME,
   password: process.env.AWS_RDS_PASSWORD,
   connectionLimit:10,
-  ssl : {
-    ca : fs.readFileSync(__dirname+"/rds-combined-ca-bundle.pem")
+  ssl:{
+    ca: fs.readFileSync(__dirname + '/rds-combined-ca-bundle.pem')
   }
 });
 module.exports = pool.promise();
